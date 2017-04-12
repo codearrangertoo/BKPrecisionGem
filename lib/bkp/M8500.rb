222ec89e739d38faf4f43f52ab3b5601fb2193c0
jgarland79 <jgarland@gmail.com>
2017-04-12 06:26:08 +0000

    flow control = none

--- lib/bkp/M8500.rb
+++ lib/bkp/M8500.rb
@@ -7,6 +7,7 @@ module BKP
 		def initialize(config=nil)
 			@config = config unless config == nil
 			@sp = SerialPort.new(@config[:port_str], @config[:baud_rate], @config[:data_bits], @config[:stop_bits], @config[:parity])
+			@sp.flow_control = SerialPort::NONE
 			@sp.binmode
 			#puts @sp.methods
 			#puts @sp.get_signals

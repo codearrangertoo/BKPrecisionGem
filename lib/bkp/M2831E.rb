a285c768958097240239b3d819e579c8f93eefa1
Jason Garland <jgarland@gmail.com>
2017-04-21 00:04:51 +0000

    use software flow control

--- lib/bkp/M2831E.rb
+++ lib/bkp/M2831E.rb
@@ -6,8 +6,7 @@ module BKP
 
     def initialize(config)        
       @sp = SerialPort.new(config[:port_str], config[:baud_rate], config[:data_bits], config[:stop_bits], config[:parity])
-      # @sp.flow_control = SerialPort::NONE
-
+      @sp.flow_control = SerialPort::SOFT
     end
 
     def cmd(command)

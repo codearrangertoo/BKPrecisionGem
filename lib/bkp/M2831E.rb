0bc3d7709fe1e80fb635aae09277b28b57d262b0
Jason Garland <jgarland@gmail.com>
2017-04-27 23:05:47 +0000

    added retries

--- lib/bkp/M2831E.rb
+++ lib/bkp/M2831E.rb
@@ -4,25 +4,27 @@ module BKP
 
   class M2831E
 
-    def initialize(config)        
+    def initialize(config)
       @sp = SerialPort.new(config[:port_str], config[:baud_rate], config[:data_bits], config[:stop_bits], config[:parity])
       @sp.flow_control = SerialPort::NONE
       @sp.read_timeout = 2000
     end
 
-    def cmd(command)
+    def cmd(command, retries = 0)
       @sp.write command + "\n"
-    	resp = @sp.gets.chomp
-    	if resp == command
-    		resp = @sp.gets.chomp
-    	end
-    	return resp
+      resp = @sp.gets("\n")
+      cmd(command, retries + 1) if resp == nil && retries <= 4
+      if resp.chomp == command
+        resp = @sp.gets("\n")
+        cmd(command, retries + 1) if resp == nil && retries <= 4
+      end
+      return resp.chomp
     end
-    
+
     def fetch()
-    	cmd(":FETCh?").to_f
+      cmd(":FETCh?").to_f
     end
-    
+
     def idn()
       cmd("*IDN?")
     end
